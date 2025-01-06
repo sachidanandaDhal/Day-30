4d2f8375fbcc958a6fc16b77b4fc95aaf5a30f84
sachidanandaDhal <dhal.sachidananda1@gmail.com>
2025-01-06 21:07:03 +0530

    DAY-1

---
+++ Day-1/GO/1.go
@@ -0,0 +1,13 @@
+package main
+
+// Every Go program starts with a package declaration.
+// package main is used for executable programs.
+
+import "fmt"
+
+// Libraries/modules are imported using the import keyword.
+// Example: import "fmt" for formatted I/O.
+
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ Day-1/JAVASCRIPT/1.js
@@ -0,0 +1,38 @@
+
+// Variables:
+
+let a = "21";
+console.log(a);
+
+const b = "20"; 
+console.log(b);
+
+var c = "40";
+console.log(c);
+
+
+// Functions:
+
+
+let ba="10";
+
+function ab(c) {
+    return `hello, ${c}!`
+}
+console.log(ab(ba))
+
+// Conditional Statements:
+
+if (a<b){
+    console.log("a is less than b")
+}else {
+    console.log("a is greater than b")
+}
+
+// loop
+
+const z =10;
+for (let i = 0; i < z; i++) {
+    console.log(i);
+  }
+  
